53be0d9fbbd1d3926fe0d5fc0681a92f021fbef9
Davidnna <davedozie1@gmail.com>
2025-01-08 13:38:13 +0100

    correct

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ let calculate = false;
 let decimal = true;
 
 function clearDisplay() {
-    display.value = '0';
+    display.innerText = '0';
     reset = false;
     decimal = true;
 }
 
 function off() {
     reset = true;
-    display.value = "";
+    display.innerText = "";
 }
 
 function deleteLast() {
     if (!reset) {
-        if (display.value.length > 1) {
-            display.value = display.value.slice(0, -1);
-            if (display.value.slice(-1) === ".") {
+        if (display.innerText.length > 1) {
+            display.innerText = display.innerText.slice(0, -1);
+            if (display.innerText.slice(-1) === ".") {
                 decimal = true;
             }
-            else if (display.value.slice(-1) === "+" || display.value.slice(-1) === "-" || display.value.slice(-1) === "*" || display.value.slice(-1) === "/" || display.value.slice(-1) === "%") {
+            else if (display.innerText.slice(-1) === "+" || display.innerText.slice(-1) === "-" || display.innerText.slice(-1) === "*" || display.innerText.slice(-1) === "/" || display.innerText.slice(-1) === "%") {
                 decimal = false;
             }
         } else {
-            display.value = '0';
+            display.innerText = '0';
         }
         if (calculate) {
             calculate = false;
@@ -37,45 +37,45 @@ function appendCharacter(char) {
     if (!reset) {
         if (calculate) {
             if (char === "+" || char === "-" || char === "*" || char === "/" || char === "%") {
-                display.value += char;
+                display.innerText += char;
                 decimal = true;
             }
             else if (char === ".") {
-                display.value = "0" + char;
+                display.innerText = "0" + char;
             }
             else {
-                display.value = char;
+                display.innerText = char;
             }
             calculate = false;
         }
         else {
             if (char === "+" || char === "-" || char === "*" || char === "/" || char === "%") {
-                if (char === "-" && display.value.slice(-1) !== "-") {
-                    if (display.value.slice(-2, -1) !== "+" && display.value.slice(-2, -1) !== "-" && display.value.slice(-2, -1) !== "*" && display.value.slice(-2, -1) !== "/" && display.value.slice(-2, -1) !== "%") {
-                        display.value += char;
+                if (char === "-" && display.innerText.slice(-1) !== "-") {
+                    if (display.innerText.slice(-2, -1) !== "+" && display.innerText.slice(-2, -1) !== "-" && display.innerText.slice(-2, -1) !== "*" && display.innerText.slice(-2, -1) !== "/" && display.innerText.slice(-2, -1) !== "%") {
+                        display.innerText += char;
                     }
                 }
-                else if (!(display.value.slice(-1) === "+" || display.value.slice(-1) === "-" || display.value.slice(-1) === "*" || display.value.slice(-1) === "/" || display.value.slice(-1) === "%")) {
-                    display.value += char;
+                else if (!(display.innerText.slice(-1) === "+" || display.innerText.slice(-1) === "-" || display.innerText.slice(-1) === "*" || display.innerText.slice(-1) === "/" || display.innerText.slice(-1) === "%")) {
+                    display.innerText += char;
                     decimal = true;
                 }
             }
             else if (char === ".") {
                 if (decimal) {
-                    if (display.value.slice(-1) === "+" || display.value.slice(-1) === "-" || display.value.slice(-1) === "*" || display.value.slice(-1) === "/" || display.value.slice(-1) === "%") {
-                        display.value += "0" + char
+                    if (display.innerText.slice(-1) === "+" || display.innerText.slice(-1) === "-" || display.innerText.slice(-1) === "*" || display.innerText.slice(-1) === "/" || display.innerText.slice(-1) === "%") {
+                        display.innerText += "0" + char
                     }
                     else {
-                        display.value += char;
+                        display.innerText += char;
                         decimal = false;
                     }
                 }
             }
-            else if (display.value === '0') {
-                display.value = char;
+            else if (display.innerText === '0') {
+                display.innerText = char;
             }
             else {
-                 display.value += char;
+                 display.innerText += char;
             }
         }
     }
@@ -84,9 +84,9 @@ function appendCharacter(char) {
 function result() {
     if (!reset) {
         try {
-            display.value = eval(display.value);
+            display.innerText = eval(display.innerText);
         } catch {
-            display.value = 'Error';
+            display.innerText = 'Error';
         }
     }
     calculate = true;
