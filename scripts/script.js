08f8c8ed8da968a2b54e0fe8df78dd3d799af6c5
Davidnna <davedozie1@gmail.com>
2025-02-27 01:03:30 +0100

    Correct Arrangement

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,20 @@ let reset = true;
 let calculate = false;
 let decimal = true;
 
+// AC
 function clearDisplay() {
     display.innerText = '0';
     reset = false;
     decimal = true;
 }
 
+// Off
 function off() {
     reset = true;
     display.innerText = "";
 }
 
+// Delete Last Inserted
 function deleteLast() {
     if (!reset) {
         if (display.innerText.length > 1) {
@@ -33,9 +36,11 @@ function deleteLast() {
     }
 }
 
+// Insert
 function appendCharacter(char) {
     if (!reset) {
         if (calculate) {
+            // When the last button pressed was equal to
             if (char === "+" || char === "-" || char === "*" || char === "/" || char === "%") {
                 display.innerText += char;
                 decimal = true;
@@ -50,6 +55,7 @@ function appendCharacter(char) {
         }
         else {
             if (char === "+" || char === "-" || char === "*" || char === "/" || char === "%") {
+                // Allow to input 2 minus signs
                 if (char === "-" && display.innerText.slice(-1) !== "-") {
                     if (display.innerText.slice(-2, -1) !== "+" && display.innerText.slice(-2, -1) !== "-" && display.innerText.slice(-2, -1) !== "*" && display.innerText.slice(-2, -1) !== "/" && display.innerText.slice(-2, -1) !== "%") {
                         display.innerText += char;
@@ -60,6 +66,7 @@ function appendCharacter(char) {
                     decimal = true;
                 }
             }
+            // Prevent from adding two dots
             else if (char === ".") {
                 if (decimal) {
                     if (display.innerText.slice(-1) === "+" || display.innerText.slice(-1) === "-" || display.innerText.slice(-1) === "*" || display.innerText.slice(-1) === "/" || display.innerText.slice(-1) === "%") {
@@ -71,6 +78,7 @@ function appendCharacter(char) {
                     }
                 }
             }
+            // Prevents from adding two zeros
             else if (display.innerText === '0') {
                 display.innerText = char;
             }
@@ -81,6 +89,7 @@ function appendCharacter(char) {
     }
 }
 
+// Shows Result
 function result() {
     if (!reset) {
         try {
